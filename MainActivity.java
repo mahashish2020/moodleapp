54db3e481f911b4da342aadcb6b33d14efff248a
mahashish2020 <68270800+mahashish2020@users.noreply.github.com>
2020-07-31 16:52:33 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -14,7 +14,7 @@
 
 // Based on the template node_modules/cordova-android/bin/templates/project/Activity.java
 
-package com.moodle.moodlemobile;
+package org.pentaclesystems.demo.lms;
 
 import android.os.Bundle;
 import android.view.KeyEvent;
